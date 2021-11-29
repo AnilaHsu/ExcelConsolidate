b813a59bbfe88309e4e99b5ed1b30e75de3bfce0
simple1017 <simple01281017@gmail.com>
2021-11-29 14:08:56 +0800

    Determine if the end of the file name is notxls, then do not execute

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from os import walk
 import os
+import glob
 
 path = ''
 df_empty = pd.DataFrame(columns=['經銷商','保卡號碼','小  計'])
@@ -8,9 +9,12 @@ df_empty = pd.DataFrame(columns=['經銷商','保卡號碼','小  計'])
 
 for root, dirs, files in walk('consolidate_finder'):
     for file in files:
+        if not file.endswith("xls"):
+            continue
+        # file_name = glob.glob(os.path.join(root,file))
+        print(os.path.join(root,file))
         df = pd.concat(pd.read_excel(os.path.join(root,file),header=[9], sheet_name=None,engine='xlrd'))
         # df.to_csv('test.csv')
-        print(os.path.join(root,file))
         print(df)
 
         new_df = df[['經銷商','保卡號碼','小  計']]
@@ -21,6 +25,5 @@ for root, dirs, files in walk('consolidate_finder'):
 df_empty.rename(columns={'保卡號碼':'保單編號/保卡號碼'},inplace=True)
 print(df_empty)
 
-# for i in range(len(df)):
-
 # df_empty.to_excel('concat_file.xls')
+
