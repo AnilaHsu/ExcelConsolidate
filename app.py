3d0095ac916c4a91b678cefbff06457a58bfe761
simple1017 <simple01281017@gmail.com>
2021-11-30 17:27:48 +0800

    add dealer_df and /opt/homebrew/bin/python3 "/Users/anila/Desktop/Excel Consolidate/app.py"

--- app.py
+++ app.py
@@ -3,27 +3,61 @@ from os import walk
 import os
 import glob
 
+from pandas.core.reshape.concat import concat
+
 path = ''
-df_empty = pd.DataFrame(columns=['經銷商','保卡號碼','小  計'])
+df_empty = pd.DataFrame(columns=['經銷商','經銷商', '保卡號碼', '小  計'])
 # print(df_empty)
 
 for root, dirs, files in walk('consolidate_finder'):
     for file in files:
         if not file.endswith("xls"):
             continue
-        # file_name = glob.glob(os.path.join(root,file))
         print(os.path.join(root,file))
-        df = pd.concat(pd.read_excel(os.path.join(root,file),header=[9], sheet_name=None,engine='xlrd'))
+        data_df = pd.concat(pd.read_excel(os.path.join(root, file),
+                       header=[9], sheet_name=None, engine='xlrd'))
         # df.to_csv('test.csv')
-        print(df)
+        # print(df)
 
-        new_df = df[['經銷商','保卡號碼','小  計']]
-        print(new_df)
-        df_empty=df_empty.append(new_df,ignore_index=True)
-        print(df_empty)
+        # 選取需要的資料至目前的 DF
+        need_df = data_df[['經銷商','保卡號碼', '小  計']]
+        # print(new_df)
+        
+        # 新建一欄經銷商(下移資料)
+        dealer_df = need_df['經銷商'].shift()
+        # print('dealer_df',dealer_df)
+        
+        # 合併新的經銷商至目前的 DF
+        shift_df = concat([dealer_df,need_df],axis=1)
 
-df_empty.rename(columns={'保卡號碼':'保單編號/保卡號碼'},inplace=True)
-print(df_empty)
+        # print(shift_df.columns)
+        # print(df_empty)
+        
+        # 轉換內外 index 
+        # df = shift_df.swaplevel()
+        
+        # 刪掉多層 index 的第一層 index
+        shift_df.index = shift_df.index.droplevel()
+        # print('shift_df',shift_df)
+      
+        shift_df.to_excel('shift_df.xls')
+        
+        odd_df = shift_df[shift_df.index%2==1]
+        odd_df.to_excel('odd.xls')
 
-# df_empty.to_excel('concat_file.xls')
+        # 彙整 Excel 
+        df_concat = df_empty.append(shift_df, ignore_index=True)
+        # print(df_concat)
+        df_concat.to_excel('test_update.xls')
 
+# df_empty.rename(columns={'保卡號碼': '保單編號/保卡號碼'}, inplace=True)
+# print(df_empty)
+
+# shift_df = concat([dealer_df,df_empty],axis=1)
+# print(df_empty)
+
+# df = df_concat[df_concat.index%2==1]
+
+
+# print(df)
+# df.to_excel('test2.xls')
